459a33845ec583bce22306c5adac18b237a3a922
amani esseili <esseiliamani@hotmail.com>
2024-06-03 10:24:00 +0200

    Created Nav component and added it to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <script>
+import NavBar from './components/NavBar.vue';
+
 	export default {
+    components: {
+      NavBar
+    }
 
 	}
 </script>
 <template>
-  <div>
+
+    <NavBar />
     <h1>Weather for Lodi Italy</h1>
     
-  </div>
+
 </template>
 
 <style lang="scss">
+
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  //name: "NavBar"
+
+}
+</script>
+
+<template>
+  
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container">
+        <!-- logo -->
+        <a class="navbar-brand" href="#">Navbar</a>
+        <!-- navBar toggler button -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- NavBr -->
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Settings</a>
+            </li>
+          </ul>
+          
+        </div>
+      </div>
+    </nav>
+    
+
+</template>
+
+<style scoped>
+.navbar-nav .nav-link {
+  cursor: pointer
+}
+</style>
